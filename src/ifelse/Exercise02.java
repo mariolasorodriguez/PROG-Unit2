d10a5c0f27352c649f20109efb556f147de2b1ef
mario.laso <mario.laso@iesnervion.es>
2025-10-16 14:40:00 +0200

    16/10

---
+++ src/ifelse/Exercise02.java
@@ -0,0 +1,16 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class Exercise02 {
+
+	public static void main(String[] args) {
+		
+		// Creation of the scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// Close the scanner
+		reader.close();
+	}
+
+}
