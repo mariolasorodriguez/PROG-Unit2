885a9b755ad6400b53b187126d2e489d51616278
mario.laso <mario.laso@iesnervion.es>
2025-10-17 13:14:01 +0200

    2, 3, 4

--- src/ifelse/Exercise02.java
+++ src/ifelse/Exercise02.java
@@ -9,6 +9,21 @@ public class Exercise02 {
 		// Creation of the scanner
 		Scanner reader = new Scanner(System.in);
 		
+		// Declaration of variables
+		double number;
+		
+		// Ask the user for the number
+		System.out.println("Introduce un numero: ");
+		// Read the input
+		number = reader.nextDouble();
+		
+		// Check if the number is close to 0
+		if (number > -1 && number < 0 || (number > 0 && number < 1)) {
+			System.out.println("Tu numero es casi 0");
+		} else {
+			System.out.println("Tu numero no es casi 0");
+		}
+		
 		// Close the scanner
 		reader.close();
 	}
