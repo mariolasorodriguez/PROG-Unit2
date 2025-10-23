703b35532818b19e151a56b67f287696dd3b74df
mario.laso <mario.laso@iesnervion.es>
2025-10-23 14:22:44 +0200

    23/10

---
+++ src/boletincondicionales/Exercise05.java
@@ -0,0 +1,23 @@
+package boletincondicionales;
+
+import java.util.Scanner;
+
+public class Exercise05 {
+
+	public static void main(String[] args) {
+		
+		// Creamos el scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// Declaramos variable para la distancia
+		double distancia;
+		
+		// Declaramos constantes
+		final double DES_ESTA_DIST = 0.30;
+		final double PRECIO_KM = 2.5;
+		
+		// Preguntamos la distancia a recorrer
+		
+	}
+
+}
