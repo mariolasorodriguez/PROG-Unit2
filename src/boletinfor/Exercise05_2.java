c4698f9433b86734d0442fdd752d7b6157fb55b9
mario.laso <mario.laso@iesnervion.es>
2025-11-11 12:29:24 +0100

    11/11

---
+++ src/boletinfor/Exercise05_2.java
@@ -0,0 +1,37 @@
+package boletinfor;
+
+import java.util.Scanner;
+
+public class Exercise05_2 {
+	public static void main(String[] args) {
+
+		// Pedir un número y calcular su factorial. Por ejemplo, el factorial de 5 se
+		// denota 5! y es igual a 5x4x3x2x1 = 120.
+
+		// Creamos el scanner
+		Scanner reader = new Scanner(System.in);
+
+		// Declaramos las variables
+		int numero;
+		long factorial = 1;
+
+		// Pedimos al usuario un numero
+		System.out.println("Introduce un numero entero para factorizar: ");
+		// Leemos el numero
+		numero = reader.nextInt();
+
+		// Bucle para calcular el factorial del numero introducido
+		for (int contador = numero; contador >= 1; contador--) {
+			System.out.print(contador);
+			if (contador != 1) {
+				System.out.print(" * ");
+			}
+			factorial *= contador;
+		}
+
+		System.out.println(" = " + factorial);
+
+		// Cerramos el scanner
+		reader.close();
+	}
+}
